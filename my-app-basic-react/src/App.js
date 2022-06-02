e944fcd38231117dc8988852993d3cbd671940c5
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-06-02 19:49:16 +0530

    changed emmet and started useState

--- my-app-basic-react/src/App.js
+++ my-app-basic-react/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // const [name, setName] = useState("jashu");
+  const [name, setName] = useState(true);
+  // var HandleNameChange = () => {
+  //   setName("Shani");
+  // };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name ? "Shani" : "Ragavendra"}</h1>
+      {/* <button onClick={HandleNameChange}>Change name</button> */}
+      <button onClick={() => setName(!name)}>Change name</button>
     </div>
   );
 }
